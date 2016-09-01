1822f63d3e5dced7a826089957bc6020538e6af4
Yin Wei <wyin@xchange.im>
2016-09-01 08:49:47 +0800

    HTTP工具：GET方法及后续的多种处理方式

---
+++ utils/http.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+import requests
+
+logger = logging.getLogger('Utils.HTTP')
+
+
+def get(url, headers=None, timeout=3, retry_times=5):
+    for i in range(retry_times):
+        try:
+            rsp = requests.get(url, headers=headers, timeout=timeout)
+            if rsp.status_code == 200:
+                return rsp
+            if i+1 <= retry_times:
+                logger.debug('远端内容异常，重试#%d %s', i+1, url)
+        except requests.exceptions.RequestException:
+            if i+1 <= retry_times:
+                logger.debug('连接远端错误，重试#%d %s', i + 1, url)
+    logger.warning('请求失败 %s', url)
+    return None
+
+
+def get_raw_content(*args, **kwargs):
+    rsp = get(*args, **kwargs)
+    if rsp is None:
+        return None
+    else:
+        return rsp.content
+
+
+def get_content(url, encoding='UTF-8', *args, **kwargs):
+    rsp = get(url, *args, **kwargs)
+    if rsp is None:
+        return None
+    else:
+        rsp.encoding = encoding
+        return rsp.text
+
+
+def get_json(url, *args, **kwargs):
+    rsp = get(url, *args, **kwargs)
+    if rsp is None:
+        return None
+    try:
+        return rsp.json()
+    except ValueError:
+        logger.warning('远端内容不是JSON %s', url)
+        return None
